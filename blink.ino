76af207bbcd8744ae00d30f6955974d290058825
Luca Viana <78035167+O-orion@users.noreply.github.com>
2025-10-03 22:18:41 -0300

    Parcial 1.4

--- blink.ino
+++ blink.ino
@@ -17,3 +17,106 @@ void loop() {
   digitalWrite(ledPin, LOW);
   while (true);  // Mantém o loop travado para que nada mais aconteça
 }
+
+// #######################################################################################################################
+// Código parcial atividade 1.4
+#define LED_PIN 15
+#define LDR_PIN 34
+
+int ledValue = 0;          
+int thresholdValue = 50;   
+const int ldrMax = 4095;   
+unsigned long lastSend = 0; 
+bool autoControlActive = false;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(LED_PIN, OUTPUT);
+  // pinMode(LDR_PIN, INPUT);
+
+  Serial.println("SmartLamp inicializada");
+  ledUpdate(); 
+}
+
+void loop() {
+  
+  if (Serial.available() > 0) {
+    String cmd = Serial.readStringUntil('\n');
+    cmd.trim();
+    processCommand(cmd);
+  }
+
+  
+  if (millis() - lastSend >= 2000) {
+    int ldrVal = ldrGetValue();
+    Serial.print("RES GET_LDR ");
+    Serial.println(ldrVal);
+    lastSend = millis();
+  }
+
+  int ldrVal = ldrGetValue();
+  if (ldrVal > thresholdValue && ledValue == 0) {
+    if (!autoControlActive) {
+      autoControlActive = true;
+      ledValue = 100;
+      ledUpdate();
+    }
+  }  else if (autoControlActive && ldrVal <= thresholdValue) {
+    autoControlActive = false;
+    ledValue = 0;
+    ledUpdate();
+  }
+}
+
+
+void processCommand(String cmd) {
+  if (cmd.startsWith("SET_LED")) {
+    int value = cmd.substring(8).toInt();
+    if (value >= 0 && value <= 100) {
+      ledValue = value;
+      autoControlActive = false;
+      ledUpdate();
+      Serial.println("RES SET_LED 1");
+    } else {
+      Serial.println("RES SET_LED -1");
+    }
+  }
+  else if (cmd == "GET_LED") {
+    Serial.print("RES GET_LED ");
+    Serial.println(ledValue);
+  }
+  else if (cmd == "GET_LDR") {
+    int ldrVal = ldrGetValue();
+    Serial.print("RES GET_LDR ");
+    Serial.println(ldrVal);
+  }
+  else if (cmd.startsWith("SET_THRESHOLD")) {
+    int value = cmd.substring(14).toInt();
+    if (value >= 0 && value <= 100) {
+      thresholdValue = value;
+      Serial.println("RES SET_THRESHOLD 1");
+    } else {
+      Serial.println("RES SET_THRESHOLD -1");
+    }
+  }
+  else if (cmd == "GET_THRESHOLD") {
+    Serial.print("RES GET_THRESHOLD ");
+    Serial.println(thresholdValue);
+  }
+  else {
+    Serial.println("Erro comando desconhecido.");
+  }
+}
+
+void ledUpdate() {
+  int pwm = map(ledValue, 0, 100, 0, 255);
+  analogWrite(LED_PIN, pwm);
+}
+
+int ldrGetValue() {
+  int raw = analogRead(LDR_PIN);
+  int norm = map(raw, 0, ldrMax, 0, 100);
+  if (norm > 100) return 100;
+  if (norm < 0) return 0;
+  return norm;
+}
